0ab24528f3111c1e1ac6412ace62dcb62fa980ce
Atef KHELIFI <atef.khelifi@esprit.tn>
2020-03-05 10:00:10 +0100

    atef commit

---
+++ src/main/java/tn/home/spring/ConsomiTounsiApplication.java
@@ -0,0 +1,13 @@
+package tn.home.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsomiTounsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsomiTounsiApplication.class, args);
+	}
+
+}
